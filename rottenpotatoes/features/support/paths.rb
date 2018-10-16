937cb35f1f239c6800f42659aa80d113e3af814c
Mia Kim <miakim@sandiego.edu>
2018-10-16 23:02:41 +0000

    Scenario 1 of part 2 finished

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -13,8 +13,10 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /^the home\s?page$/
-      '/'
+    when /^the (RottenPotatoes) home\s?page$/ then '/movies'
+    when /^the movies page&/ then '/movies'
+    when /^the edit page for "(.*)"$/ 
+       edit_polymorphic_path(Movie.where("title = ?", $1).first)
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
